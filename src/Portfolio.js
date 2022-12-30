cec18db114a896a65dc1c0707b27403efc686b93
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2022-12-30 22:13:31 +0100

    url update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,7 +7,7 @@ const Portfolio = () => {
         const [portfolio, setPortfolio] = useState([
          {title: "Github Portfolio", body: "Lorem ipsum...", author: "EMJcreates", url:"https://jewelaltschool2ndsemesterexam.netlify.app/", id: 1, },
          {title: "Book App", body: "Lorem ipsum...", author: "EMJcreates", url: "https://book-app-olive.vercel.app/", id: 2},
-         {title: "Reciepe Website", body: "Lorem ipsum...", author: "EMJcreates", url:"https://food-website-tau.vercel.app/", id: 3, },
+         {title: "Reciepe Website", body: "Lorem ipsum...", author: "EMJcreates", url:"https://food-website-byemjcreates.vercel.app/", id: 3, },
          {title: "Party Website", body: "Lorem ipsum...", author: "EMJcreates", url: "https://altschoolreactrouterassignment.netlify.app/",  id: 4},
          {title: "Calculator App", body: "Lorem ipsum...", author: "EMJcreates", url: "https://Altschool-Calculator-App-Assignment.emjcreates.repl.co",  id: 5},
         ]);
